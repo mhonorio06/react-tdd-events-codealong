4cdb3c7b51258eafff3282f5d3fd72b6072a55a9
Manny <mhfernan35277@gmail.com>
2024-03-20 22:52:28 -0700

    adds file

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
+import { useState } from "react";
 function App() {
+  const [pepperoniChecked, setPepperoniChecked] = useState(false)
+  function togglePepperoni(e) {
+    setPepperoniChecked(e.target.checked);
+  }
   return (
-    <div></div>
+    <div>
+        <h1>Select Pizza Toppings</h1>
+      <input
+        type="checkbox"
+        id="pepperoni"
+        checked={pepperoniChecked}
+        aria-checked={pepperoniChecked}
+        onChange={togglePepperoni}
+      />
+      <label htmlFor="pepperoni">Add pepperoni</label>
+
+      <h2>Your Toppings:</h2>
+      <ul>
+        <li>Cheese</li>
+        {pepperoniChecked ? <li>Pepperoni</li> : null}
+      </ul>
+    </div>
   )
 }
 
